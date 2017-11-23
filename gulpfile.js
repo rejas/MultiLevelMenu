93c8fd6aa44909bb1752823674139db756089c89
Veeck <veeck@neofonie.de>
2017-11-23 12:31:57 +0100

    Add proper structure

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp            = require('gulp'),
+    connect         = require('gulp-connect'),
+    csslint         = require('gulp-csslint'),
+    eslint          = require('gulp-eslint'),
+    lintspaces      = require('gulp-lintspaces'),
+    rename          = require('gulp-rename'),
+    uglify          = require('gulp-uglify'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    cleanCSS        = require('gulp-clean-css');
+
+gulp.task('csslint', function() {
+    return gulp.src('src/MultiLevelMenu.css')
+        .pipe(csslint('.csslintrc'))
+        .pipe(csslint.formatter());
+});
+
+gulp.task('copy:css', ['csslint'], function() {
+    return gulp.src('src/MultiLevelMenu.css')
+        .pipe(gulp.dest('docs/css/'));
+});
+
+gulp.task('minify:css', ['copy:css'], function() {
+    return gulp.src('src/MultiLevelMenu.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9', 'Firefox ESR', 'Android >= 2.3'],
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(rename('MultiLevelMenu.min.css'))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('editorconfig', function() {
+    return gulp.src('src/MultiLevelMenu.js')
+        .pipe(lintspaces({editorconfig: './.editorconfig'}))
+        .pipe(lintspaces.reporter());
+});
+
+gulp.task('eslint', function() {
+    return gulp.src('src/MultiLevelMenu.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('copy:js', ['editorconfig', 'eslint'], function() {
+    return gulp.src('src/MultiLevelMenu.js')
+        .pipe(gulp.dest('docs/javascripts/'));
+});
+
+gulp.task('minify:js', ['copy:js'], function() {
+    return gulp.src('src/MultiLevelMenu.js')
+        .pipe(uglify())
+        .pipe(rename('MultiLevelMenu.min.js'))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch([
+        'docs/*.html',
+        'src/**/*'
+    ], ['build']);
+});
+
+gulp.task('serve', ['build', 'watch'], function() {
+    connect.server({
+        livereload: true
+    });
+});
+
+gulp.task('build', ['minify:css', 'minify:js']);
+
+gulp.task('default', ['build']);
